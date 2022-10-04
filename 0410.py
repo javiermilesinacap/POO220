b24d41c0d99476a61229c59d7b07d78b2645d718
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-04 14:48:11 -0300

    Update 0410.py

--- 0410.py
+++ 0410.py
@@ -16,8 +16,11 @@ class Persona:
     nombre = ""
     def mostrarNombre(self):
         print("Mi nombre es: "+self.nombre)
-    def __init__(self):
-        self.edad = 0
+    def mostrarDatos(self):
+        print("La persona se llama "+ str(self.nombre) + "de edad:"+ str(self.edad))
+    def __init__(self, nombre ="", edad=0):
+        self.edad = edad
+        self.nombre = nombre
 objeto1 = UnaClase()
 objeto2 = UnaClase()
 objeto1.atributo = 999
@@ -31,3 +34,5 @@ print("LA edad de la persona 1 es: "+ str(persona1.edad))
 persona1.mostrarNombre()
 persona2.mostrarNombre()
 print(persona1)
+persona3 = Persona(22,"Juan")
+persona3.mostrarDatos()
