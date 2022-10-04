1aef9b664fea39ff70f5a16756a04a4da3e8141b
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-04 14:39:20 -0300

    Introducción a Clases

---
+++ 0410.py
@@ -0,0 +1,33 @@
+#Variables
+a = 123
+b = "hola mundo"
+#Funciones
+def mensaje():
+    print("Esto es un mensaje")
+def mensaje1():
+    print("Esto es una variable"+b)
+mensaje()
+mensaje1()
+#DESDE ACA COMIENZA LA LECCION DE CLASES.
+class UnaClase:
+    atributo = 123
+    atributo2 = 456
+class Persona: 
+    nombre = ""
+    def mostrarNombre(self):
+        print("Mi nombre es: "+self.nombre)
+    def __init__(self):
+        self.edad = 0
+objeto1 = UnaClase()
+objeto2 = UnaClase()
+objeto1.atributo = 999
+objeto2.atributo = 888
+persona1 = Persona()
+persona1.nombre = "Benjamin"
+persona2 = Persona()
+persona2.nombre = "Miguel"
+print("Existen dos personas: "+ persona1.nombre +" y "+persona2.nombre)
+print("LA edad de la persona 1 es: "+ str(persona1.edad))
+persona1.mostrarNombre()
+persona2.mostrarNombre()
+print(persona1)
