621c2870ba9d42859b67bb1daf2d23fbd14a7309
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-18 14:31:00 -0300

    funcionalidad

--- proyecto1110.py
+++ proyecto1110.py
@@ -1,7 +1,7 @@
 import math
 import pygame
 class Vehiculo:
-    def __init__(self,velocidad=0, lat=0.0, lon=0.0):
+    def __init__(self,velocidad=0, lat=0.0, lon=300.0):
         self.velocidad = velocidad
         self.lat = lat
         self.lon = lon
@@ -28,19 +28,38 @@ class Vehiculo:
         latD = float(input("Ingrese lat destino en decimal"))
         lonD = float(input("Ingrese lon destino en decimal"))
         circunf = math.pi * latD * 2
+class Carretera: 
+    velocidad = 0
+    def __init__(self, velocidad=0) -> None:
+        self.velocidad = velocidad
+    def getVelocidad(self):
+        return self.velocidad
+    def setVelocidad(self,velocidad):
+        self.velocidad = velocidad
+
 objeto = Vehiculo()
+fondo = Carretera()
 pygame.init()
 ventana = pygame.display.set_mode((800,600))
 autito = pygame.image.load("./car.png").convert_alpha()
+carretera = pygame.image.load("./fondo.png").convert_alpha()
 repr(objeto)
 while(True):
     for event in pygame.event.get():
         if(event.type == pygame.KEYDOWN and event.key == pygame.K_w):
             objeto.acelerar()
+            
             print("la velocidad del vehiculo es: "+objeto.velocidad.__str__())
         if(event.type == pygame.KEYDOWN and event.key == pygame.K_s):
             objeto.frenar()
+            
             print("la velocidad del vehiculo es: "+objeto.velocidad.__str__())
+
         ventana.fill((255,255,255))
+        ventana.blit(carretera,(fondo.velocidad,0))
         ventana.blit(autito,(objeto.lat,objeto.lon))
-        pygame.display.update()
+    fondo.velocidad -= (objeto.velocidad/5)
+    pygame.display.update()
+            
+    if(fondo.velocidad<-300):
+        fondo.velocidad=0
