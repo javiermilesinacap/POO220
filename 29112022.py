f4c42b1dafa5abab624bba212ecc363dd28ad518
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-11-29 16:50:02 -0300

    Update 29112022.py

--- 29112022.py
+++ 29112022.py
@@ -12,43 +12,70 @@ for row in c:
 '''
 import tkinter as tk
 from tkinter import ttk
-class Sistema():
-    def __init__(self):
-        self.ventana = tk.Tk()
-        self.ventana.geometry("800x600")
-    def login(self):
-        self.estilo = ttk.Style
-        self.etiqueta = tk.Label(text="hola")
-        self.etiqueta.grid(row=0,column=0)
-class Menu(tk.Frame):
+
+
+class InputFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        # Username
+        ttk.Label(self, text='Acceder').grid(column=0, row=0, sticky=tk.W)
+        self.cajaUsername = ttk.Entry(self, width=30)
+        self.cajaUsername.focus()
+        self.cajaUsername.grid(column=1, row=0, sticky=tk.W)
+
+        # Password:
+        self.cajaPassword = ttk.Label(self, text='Password').grid(column=0, row=1, sticky=tk.W)
+        self.cajaPassword = ttk.Entry(self, width=30)
+        self.cajaPassword.grid(column=1, row=1, sticky=tk.W)
+
+        boton = ttk.Button(text="Acceder", command=lambda: self.ValidarUsuario(self.cajaUsername.get(), self.cajaPassword.get()))
+        boton.grid(row=3,column=1)
+        for widget in self.winfo_children():
+            widget.grid(padx=0, pady=5)
+    def ValidarUsuario(self, username, password):
+        print("Conectando a Oracle")
+        sql = "SELECT * from usuaruio where username '"+username+' and pass......etc.'
+        print("Validando datos username..."+username)
+        print("Validando datos password..."+password)
+        print("ACA VA LA CONSULTA Y LA VALIDACION DEL USUARIO" + sql)
+
+'''
+class ButtonFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        # setup the grid layout manager
+        self.columnconfigure(0, weight=1)
+
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        ttk.Button(self, text='Accederd', command=lambda: print("BOTON CLICK")).grid(column=0, row=3)
+
+        for widget in self.winfo_children():
+            widget.grid(padx=0, pady=3)
+'''
+
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.ventana = tk.Tk()
-        self.ventana.geometry("800x600")
-        self.ventana.title("Menu del Sistema")
-class Usuario: 
-    def addUser(self):
-        sql = "INSERT INTO ..-..."
-        #conexion.execute(sql)
-main = Sistema()
-main.login()
-if(True):
-    menu = Menu()
-main.ventana.mainloop()
+        self.title('User Login')
+        self.geometry('400x150')
+        self.resizable(0, 0)
+        self.__create_widgets()
 
-# Create Tkinter Object
-root = tk.Tk()
+    def __create_widgets(self):
+        # create the input frame
+        input_frame = InputFrame(self)
+        input_frame.grid(column=0, row=0)
 
-# Set Geometry
-root.geometry("400x400")
+        # create the button frame
+        #button_frame = ButtonFrame(self)
+        #button_frame.grid(column=1, row=0)
 
-# Frame 1
-frame1 = tk.Frame(root,bg="black",width=500,height=300)
-frame1.pack()
 
-# Frame 2
-frame2 = tk.Frame(frame1,bg="red",width=100,height=100)
-frame2.pack(pady=20,padx=20)
-
-# Execute Tkinter
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
